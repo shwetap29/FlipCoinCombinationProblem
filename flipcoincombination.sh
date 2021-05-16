7d977f2dc58bf62f3f0798d02df3caee95dcce79
shwetap29 <shwetaprasad48@gmail.com>
2021-05-16 15:27:14 +0530

    multiple times of fliping coin and finding percentage in flipcoincombination.sh

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -10,29 +10,52 @@ read -p "Enter how many time to flip coin: " userInput
 TEMP=$userInput
 #INITILIZE DICTIONARY
 declare -A FlipingCoin
+#constant variable
+COUNT_HH=0
+COUNT_HT=0
+COUNT_TT=0
+COUNT_TH=0
+#INITILIZE DICTIONARY
+declare -A FlipingCoin
 #USING LOOP TO FLIP COIN MULTIPLE TIME
 for(( i=1; i<=$userInput; i++ ))
 	do
 		#GENERATING RANDOM VALUE
 		result=$((RANDOM%2+1))
+                 #USING CASESTATEMENT
 		case $result in
 			1)
-				COUNT_HEAD=$(($COUNT_HEAD+1))
-				FlipingCoin["H"]="$COUNT_HEAD"
+				COUNT_HH=$(($COUNT_HH+1))
+				FlipingCoin["HH"]="$COUNT_HH"
 				;;
 			2)
-				COUNT_TAIL=$(($COUNT_TAIL+1))
-				FlipingCoin["T"]="$COUNT_TAIL"
+				COUNT_HT=$(($COUNT_HT+1))
+				FlipingCoin["HT"]="$COUNT_HT"
 				;;
+                        3)
+                                COUNT_TT=$(($COUNT_TT+1))
+                                FlipingCoin["TT"]="$COUNT_TT"
+                                ;;
+                        2)
+                                COUNT_TH=$(($COUNT_TH+1))
+                                FlipingCoin["TH"]="$COUNT_TH"
+                                ;;
+
 		esac
 	done
-headPercentage=`echo "scale=3; $COUNT_HEAD / $TEMP * 100 " |bc -l`
-tailPercentage=`echo "scale=3; $COUNT_TAIL / $TEMP * 100 " |bc -l`
+#CALCULATING PERCENTAGE OF FLIPING COIN
+HeadHeadPercentage=`echo "scale=3; $COUNT_HH / $TEMP * 100 " #|bc -l`
+HeadTailPercentage=`echo "scale=3; $COUNT_HT / $TEMP * 100 " #|bc -l`
+TailTailPercentage=`echo "scale=3; $COUNT_TH / $TEMP * 100 " #|bc -l`
+TailHeadPercentage=`echo "scale=3; $COUNT_TH / $TEMP * 100 "  #|bc -l`
+
 #PRINTING KEY AND VALUE OF COIN
 for count in ${!FlipingCoin[@]}
 	do
 		echo "$count : ${FlipingCoin[$count]}"
 	done
 #PRINTING PERSENTAGE OF FLIPING COIN
-echo "Head percentage : $headPercentage "
-echo "Tail percentage : $tailPercentage "
+echo "HeadHead percentage : $HeadHeadPercentage "
+echo "HeadTail percentage : $HeadTailPercentage "
+echo "TailTail percentage : $TailTailPercentage "
+echo "TailHead percentage : $TailHeadPercentage "
